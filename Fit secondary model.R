ba624f294b55d62714a45bb1740e16d5b53630c4
jun3248 <js3248@cornell.edu>
2022-12-21 11:27:37 -0500

    Fit secondary model

--- Fit secondary model.R
+++ Fit secondary model.R
@@ -1,15 +1,15 @@
 data <- read.csv("mumax.csv")
 colnames(data) <- c("Isolate","T","mumax","sqrt_mumax")
 
-Iso413<-subset(data,Isolate == "413")
-fit <- lm(Iso413$sqrt_mumax[2:4] ~ Iso413$T[2:4])
-plot(Iso413$T,Iso413$sqrt_mumax,
+Iso193<-subset(data,Isolate == "193")
+fit <- lm(Iso193$sqrt_mumax ~ Iso193$T)
+plot(Iso193$T,Iso193$sqrt_mumax,
      ylim = c(0,1.5),
      xlim = c(0,24))
 abline(fit)
 summary(fit)
 
-sqrt_mumax = 0.020914*T + 0.145254
+sqrt_mumax = 0.04495 *T - 0.20936
   
-b_413 <- coef(fit) [2]
-Tmin_413 <- -0.145254/0.020914
+b_193 <- coef(fit) [2]
+Tmin_193 <- 0.20936/0.04495
