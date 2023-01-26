8ba3e4fe955d74c86b87542979e02f7e8e938cb6
jun3248 <js3248@cornell.edu>
2023-01-26 14:27:14 -0500

    Add growth/no growth data

--- Fit secondary model.R
+++ Fit secondary model.R
@@ -1,15 +1,18 @@
 data <- read.csv("mumax_R.csv")
 colnames(data) <- c("Isolate","T","mumax","sqrt_mumax")
 
-Iso649<-subset(data,Isolate == "649")
-fit <- lm(Iso649$sqrt_mumax ~ Iso649$T)
-plot(Iso457$T,Iso457$sqrt_mumax,
-     ylim = c(0,4),
-     xlim = c(0,24))
+Iso194<-subset(data,Isolate == "194")
+fit <- lm(Iso194$sqrt_mumax ~ Iso194$T)
+plot(Iso194$T,Iso194$sqrt_mumax,
+     ylim = c(0,5),
+     xlim = c(0,24),
+     main = "Isolate 194 Secondary Model",
+     xlab = "Temperature, °C",
+     ylab = "Square root of mumax, ln CFU/mL h-1)1/2")
 abline(fit)
 summary(fit)
 
-sqrt_mumax = 0.067519 *T + 0.468946
+sqrt_mumax = 0.12459 *T - 0.65114
   
 b_649 <- coef(fit) [2]
-Tmin_407 <- -0.468946/0.067519
+Tmin_649 <- 0.65114/0.12459
